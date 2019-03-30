96fffd86680af4b5700842dc1f8f7c3933c749ad
rmisys123 <sapnil123@yahoo.com>
2019-03-31 02:36:25 +0600

    sucessfully load the train model

--- Sentiemnt-train-model/train_model/TFIDF_nativebayes.py
+++ Sentiemnt-train-model/train_model/TFIDF_nativebayes.py
@@ -13,11 +13,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+import pickle  
+from nltk.corpus import stopwords  
 
 documents = []
 #nltk.download('stopwords')  
-import pickle  
-from nltk.corpus import stopwords  
+
 
 movie_data = load_files(r"E:\chatbott\githubrepro\Sentiemnt-analysis\Nltk-sentiment-analysis\Nltk-sentiment-analysis\Sentiemnt-train-model\train_model\txt_sentoken")  
 #print("the len value is ", len(movie_data.data))
@@ -56,10 +57,6 @@ for sen in range(0, len(X)):
     
     # convert text documents into TFIDF feature values
      
-   # tfidfconverter = TfidfVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))  
-    
-   # tfidfconverter = TfidfVectorizer(max_features=1500, min_df=0, max_df=1, stop_words=stopwords.words('english'))  
-
 tfidfconverter = TfidfVectorizer(max_features=2000)
 X = tfidfconverter.fit_transform(documents).toarray()  
 
@@ -70,26 +67,26 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 #split Training and Testing Sets
  
 #Training Text Classification Model and Predicting Sentiment
-"""
-classifier = RandomForestClassifier(n_estimators=1000, random_state=0)  
-classifier.fit(X_train, y_train)  
-y_pred = classifier.predict(X_test)  
-"""
+
+#classifier = RandomForestClassifier(n_estimators=1000, random_state=0)  
+#classifier.fit(X_train, y_train)  
+#y_pred = classifier.predict(X_test)  
+
 gnb = GaussianNB()
 gnb.fit(X_train, y_train)
 y_pred = gnb.predict(X_test)
 #Training Text Classification Model and Predicting Sentiment
  
 #Evaluating the Model
-print(confusion_matrix(y_test,y_pred))  
-print(classification_report(y_test,y_pred))  
-print(accuracy_score(y_test, y_pred))  
+#print(confusion_matrix(y_test,y_pred))  
+#print(classification_report(y_test,y_pred))  
+print("the accuracy level is ",accuracy_score(y_test, y_pred))  
 #Evaluating the Model
  
 #Saving and Loading the Model
-"""
+
 with open('text_classifier', 'wb') as picklefile:  
-    pickle.dump(classifier,picklefile)
+    pickle.dump(gnb,picklefile)
  
 #Saving and Loading the Model
 
@@ -101,9 +98,10 @@ with open('text_classifier', 'rb') as training_model:
 y_pred2 = model.predict(X_test)
 
 
-print(confusion_matrix(y_test, y_pred2))  
-print(classification_report(y_test, y_pred2))  
-print(accuracy_score(y_test, y_pred2))  
-"""
+
+#print(confusion_matrix(y_test, y_pred2))  
+#print(classification_report(y_test, y_pred2))  
+print("the accuracy level after load ",accuracy_score(y_test, y_pred2))  
+
 if __name__ == '__main__':
     pass
